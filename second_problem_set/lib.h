80d14fc70f47afae5af72f99dfaba01d48aafbb0
server-of-disaster <bartoszkaszuba1@gmail.com>
2020-03-17 13:49:54 +0100

    Implemented copying and random generation

---
+++ second_problem_set/lib.h
@@ -0,0 +1,20 @@
+#ifndef files_library
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "sys/stat.h"
+
+
+#define files_library
+
+int generate_random_strings(char * filename, int string_count, int string_len);
+int sort_strings_in_file_sys(char * filename, int string_count, int string_len);
+int sort_strings_in_file_lib(char * filename, int string_count, int string_len);
+int copy_file_sys(char * file1, char * file2, int string_count, int string_len);
+int copy_file_lib(char * file1, char * file2, int string_count, int string_len);
+
+#endif
+
